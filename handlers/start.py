bf23d44d4fefa43a33ef8c0168c9ecc343e60a72
aimindpulse07-spec <aimindpulse07@gmail.com>
2026-08-10 20:37:39 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,6 +3,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from keyboards.main import main_menu
+from utils.database import add_or_update_user
 
 router = Router()
 
@@ -23,6 +24,15 @@ Choose an option below.
 
 @router.message(CommandStart())
 async def start_handler(message: Message) -> None:
+    user = message.from_user
+
+    if user is not None:
+        add_or_update_user(
+            user_id=user.id,
+            username=user.username,
+            first_name=user.first_name,
+        )
+
     await message.answer(
         WELCOME_TEXT,
         reply_markup=main_menu(),
