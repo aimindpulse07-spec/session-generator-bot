03bdae58fc63924ee5e7cbf93819715451678138
aimindpulse07-spec <aimindpulse07@gmail.com>
2026-08-10 20:53:28 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,21 @@
+import logging
+import sys
 
+
+def setup_logging() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format=(
+            "%(asctime)s | "
+            "%(levelname)s | "
+            "%(name)s | "
+            "%(message)s"
+        ),
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+        ],
+        force=True,
+    )
+
+
+logger = logging.getLogger("session-generator-bot")
