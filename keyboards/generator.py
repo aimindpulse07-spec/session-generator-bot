ba250b85e9fae0504eee6eaa10c377cae8b5cb8d
aimindpulse07-spec <aimindpulse07@gmail.com>
2026-08-10 20:10:32 +0530

    Update generator.py

--- keyboards/generator.py
+++ keyboards/generator.py
@@ -1,1 +1,24 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+def library_menu() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="📱 Pyrogram",
+                    callback_data="library_pyrogram",
+                ),
+                InlineKeyboardButton(
+                    text="📡 Telethon",
+                    callback_data="library_telethon",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="✖️ Close",
+                    callback_data="close",
+                ),
+            ],
+        ]
+    )
